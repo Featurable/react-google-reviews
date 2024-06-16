45482579e2f3397b6ff31b2c4f20f7b147a2cab4
ryanchiang <ryanschiang@gmail.com>
2024-06-16 16:12:34 -0700

    add bundle analyzer

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import terser from "@rollup/plugin-terser";
 import typescript from "@rollup/plugin-typescript";
+import analyze from 'rollup-plugin-analyzer';
 import dts from "rollup-plugin-dts";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import postcss from "rollup-plugin-postcss";
@@ -37,6 +38,9 @@ export default [
         minimize: true,
       }),
       terser(),
+      analyze({
+        summaryOnly: true,
+      })
     ],
   },
   {
