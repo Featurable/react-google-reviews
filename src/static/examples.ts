0882bc94a8bf87512561c8ac4063a62a48b80d60
ryanchiang <ryanschiang@gmail.com>
2025-01-30 17:04:26 -0800

    add example reviews for dev env

---
+++ src/static/examples.ts
@@ -0,0 +1,82 @@
+import { ReactGoogleReview } from "..";
+
+export const EXAMPLE_REVIEWS: ReactGoogleReview[] = [
+    {
+        reviewId: "1",
+        reviewer: {
+            displayName: "Isabella Harris",
+            profilePhotoUrl: "",
+            isAnonymous: false,
+        },
+        comment:
+            "I was hesitant to invest in this product at first, but I'm so glad I did. It has been a total game-changer for me and has made a significant positive impact on my work. It's worth every penny.",
+        starRating: 5,
+        createTime: new Date().toISOString(),
+        updateTime: new Date().toISOString(),
+    },
+    {
+        reviewId: "2",
+        reviewer: {
+            displayName: "Sophia Moore",
+            profilePhotoUrl: "",
+            isAnonymous: false,
+        },
+        comment:
+            "I've tried similar products in the past, but none of them compare to this one. It's in a league of its own in terms of functionality, durability, and overall value. I can't recommend it highly enough!",
+        starRating: 5,
+        createTime: new Date().toISOString(),
+        updateTime: new Date().toISOString(),
+    },
+    {
+        reviewId: "3",
+        reviewer: {
+            displayName: "John Doe",
+            profilePhotoUrl: "",
+            isAnonymous: false,
+        },
+        comment:
+            "This product is a game-changer! I've been using it for a few months now and it has consistently delivered excellent results. It's easy to use, well-designed, and built to last.",
+        starRating: 5,
+        createTime: new Date().toISOString(),
+        updateTime: new Date().toISOString(),
+    },
+    {
+        reviewId: "4",
+        reviewer: {
+            displayName: "Emily Davis",
+            profilePhotoUrl: "",
+            isAnonymous: false,
+        },
+        comment:
+            "I've been using this product for a few weeks now and I'm blown away by how well it works. It's intuitive, easy to use, and has already saved me a ton of time. I can't imagine going back to the way I used to do things before I had this product. I've been using it for a few months now and it has consistently delivered excellent results. It's easy to use, well-designed, and built to last.",
+        starRating: 5,
+        createTime: new Date().toISOString(),
+        updateTime: new Date().toISOString(),
+    },
+    {
+        reviewId: "5",
+        reviewer: {
+            displayName: "David Wilson",
+            profilePhotoUrl: "",
+            isAnonymous: false,
+        },
+        comment:
+            "I was skeptical at first, but after using this product for a while, I'm a believer. It's well-designed, durable, and does exactly what it's supposed to do. I couldn't be happier with my purchase.",
+        starRating: 5,
+        createTime: new Date().toISOString(),
+        updateTime: new Date().toISOString(),
+    },
+    {
+        reviewId: "6",
+        reviewer: {
+            displayName: "Jessica Brown",
+            profilePhotoUrl: "",
+            isAnonymous: false,
+        },
+        comment:
+            "I love this product! It has exceeded my expectations in every way. Setup was a breeze and it works flawlessly. I've already recommended it to several friends and family members.",
+        starRating: 5,
+        createTime: new Date().toISOString(),
+        updateTime: new Date().toISOString(),
+    },
+];
